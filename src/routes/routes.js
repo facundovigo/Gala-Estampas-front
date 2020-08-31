24d13d303525d118c8d2d84f9c8f1a739b66c3cb
sirDemian <sirdemian@gmail.com>
2020-08-30 22:18:15 -0300

    fix eslint warn

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,32 +1,32 @@
 import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
-
 import Dashboard from "@/pages/Dashboard.vue";
 import UserProfile from "@/pages/UserProfile.vue";
-import Cardetails from "@/components/Cards/CardDetail.vue"
+import Cardetails from "@/components/Cards/CardDetail.vue";
 
-
-const routes = [{
-    mode: 'history',
+const routes = [
+    {
+    mode: "history",
     path: "/",
     component: DashboardLayout,
     redirect: "/dashboard",
-    children: [{
-            path: "dashboard",
-            name: "Dashboard",
-            component: Dashboard
-        },
-        {
-            path: "user",
-            name: "User Profile",
-            component: UserProfile
-        },
-        {
-            path: "cardDetails",
-            name: "cardDetails",
-            component: Cardetails
-        }
-
+    children: [
+    {
+        path: "dashboard",
+        name: "Dashboard",
+        component: Dashboard
+    },
+    {
+        path: "user",
+        name: "User Profile",
+        component: UserProfile
+    },
+    {
+        path: "cardDetails",
+        name: "cardDetails",
+        component: Cardetails
+    }
     ]
-}];
+    }
+];
 
 export default routes;
