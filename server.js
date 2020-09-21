a59f19b2a6fce0843ab6ab2ba855a6960625a6d6
facundo <facundovigo@gmail.com>
2020-09-21 15:16:08 -0300

    Adding files.

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const path = require('path');
 const app = express();
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+// app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-});
+// // this * route is to serve project on different page routes except root `/`
+// app.get(/.*/, function(req, res) {
+//     res.sendFile(path.join(__dirname, '/dist/index.html'))
+// });
 
-const port = process.env.PORT || 8081;
+// const port = process.env.PORT || 8081;
 
+// app.listen(port);
+
+//app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 8081;
 app.listen(port);
