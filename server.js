31e87a7962b7271ba5ab1b3bb13008de70ef551d
facundo <facundovigo@gmail.com>
2020-09-21 15:24:15 -0300

    Fix Heroku

--- server.js
+++ server.js
@@ -3,20 +3,6 @@ const serveStatic = require('serve-static');
 const path = require('path');
 
 const app = express();
-
-//here we are configuring dist to serve app files
-// app.use('/', serveStatic(path.join(__dirname, '/dist')));
-
-// // this * route is to serve project on different page routes except root `/`
-// app.get(/.*/, function(req, res) {
-//     res.sendFile(path.join(__dirname, '/dist/index.html'))
-// });
-
-// const port = process.env.PORT || 8081;
-
-// app.listen(port);
-
-//app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 8081;
 app.listen(port);
